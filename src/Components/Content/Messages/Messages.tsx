1bfaf05f4791ba01a2b23f03dbfd597456fdd54d
AlekseiCherenkevich <alekseicherenkevich@gmail.com>
2022-09-25 20:50:33 +0300

    add navlinks to messages component

--- src/Components/Content/Messages/Messages.tsx
+++ src/Components/Content/Messages/Messages.tsx
@@ -1,6 +1,7 @@
 import classes from './Messages.module.css'
 import {createRef} from "react";
-import {FriendType, MessageType, sendMessage, updateNewMessageText} from "../../../redux/messages-reducer";
+import {FriendType, MessageType} from "../../../redux/messages-reducer";
+import { NavLink } from 'react-router-dom';
 
 
 
@@ -23,10 +24,10 @@ function Messages(props: MessagesPropsType) {
     return <div className={classes.messages}>
         <div className={classes.chat}>
             <div className={classes.friends}>
-                {props.friends.map((f: any) => <div>{f.name}</div>)}
+                {props.friends.map((f: any) => <NavLink to={`/messages/${f.id}`} className={classes.friend}>{f.name}</NavLink>)}
             </div>
             <div className={classes.messages}>
-                {props.messages.map((m: any) => <div>{m.message}</div>)}
+                {props.messages.map((m: any) => <div className={classes.message}>{m.message}</div>)}
             </div>
         </div>
         <div className={classes.new_message}>
