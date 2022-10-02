ff8fffc7d2651b53c6555fa636c465eeedde4da8
AlekseiCherenkevich <alekseicherenkevich@gmail.com>
2022-10-02 12:02:20 +0300

    pass props to profile info component

--- src/Components/Content/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/Components/Content/Profile/ProfileInfo/ProfileInfo.tsx
@@ -1,27 +1,33 @@
 import classes from './ProfileInfo.module.css'
+import {ProfileType} from "../../../../redux/profile-reducer";
 
-function ProfileInfo() {
+interface IProfileInfoProps {
+    profile: ProfileType
+}
+
+function ProfileInfo(props: IProfileInfoProps) {
+    console.log(props.profile)
     return <div className={classes.profile_info}>
         <div className={classes.left_side}>
-            <h1>Name...</h1>
-            <div className={classes.avatar}></div>
+            <h1>{props.profile.fullName}</h1>
+            {props.profile.photos.large
+                ? <div style={{backgroundImage: `url("${props.profile.photos.large}")`}} className={classes.avatar}></div>
+                : <div className={classes.avatar}></div>
+            }
         </div>
         <div className={classes.right_side}>
-            <div>Full name: Test</div>
-            <div>Looking for a job: yes</div>
-            <div>My professional skills: ...</div>
-            <div>About me: test</div>
-            <ul className={classes.contacts}>Contacts:
-                <li>facebook:</li>
-                <li>website:</li>
-                <li>vk:</li>
-                <li>twitter:</li>
-                <li>instagram:</li>
-                <li>youtube:</li>
-                <li>github:</li>
-                <li>mainLink:</li>
+            <div><b>Looking for a job</b>: {props.profile.lookingForAJob ? props.profile.lookingForAJobDescription : 'No'}</div>
+            <div><b>About me</b>: {props.profile.aboutMe}</div>
+            <ul className={classes.contacts}><b>Contacts</b>:
+                <li><b>facebook</b>: {props.profile.contacts.facebook}</li>
+                <li><b>website</b>: {props.profile.contacts.website}</li>
+                <li><b>vk</b>: {props.profile.contacts.vk}</li>
+                <li><b>twitter</b>: {props.profile.contacts.twitter}</li>
+                <li><b>instagram</b>: {props.profile.contacts.instagram}</li>
+                <li><b>youtube</b>: {props.profile.contacts.youtube}</li>
+                <li><b>github</b>: {props.profile.contacts.github}</li>
+                <li><b>mainLink</b>: {props.profile.contacts.mainLink}</li>
             </ul>
-            <div>Status: Hi everybody! I'm looking for a job</div>
         </div>
     </div>
 }
