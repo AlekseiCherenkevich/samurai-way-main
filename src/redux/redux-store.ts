c996f87b0c48fbf84eeef6ecb729027f4b2459d6
AlekseiCherenkevich <alekseicherenkevich@gmail.com>
2022-10-23 19:02:48 +0300

    add formReducer to store

--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -4,17 +4,22 @@ import {messagesReducer} from "./messages-reducer";
 import {usersReducer} from "./users-reducer"
 import {authReducer} from "./auth-reducer";
 import thunkMiddleware from "redux-thunk"
+import {reducer as formReducer} from "redux-form";
 
 const rootReducer = combineReducers({
     profilePage: profileReducer,
     messagesPage: messagesReducer,
     usersPage: usersReducer,
-    auth: authReducer
+    auth: authReducer,
+    form: formReducer
 })
 
 const store = createStore(rootReducer, applyMiddleware(thunkMiddleware))
 
 export type RootReducerType = typeof rootReducer
 export type AppStateType = ReturnType<RootReducerType>
+
+//@ts-ignore
+window.store=store
 
 export default store
